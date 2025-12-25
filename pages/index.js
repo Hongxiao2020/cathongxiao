7a1be8b364cfde1359e5d418320b18bbe99e2bee
Hongxiao <yuhongxiao1991@gmail.com>
2025-12-24 23:25:28 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ const Home = () => (
             overflow="hidden"
           >
             <Image
-              src="/images/profile.png"
+              src="/images/profile.jpg"
               alt="Profile image"
               width="100"
               height="100"
